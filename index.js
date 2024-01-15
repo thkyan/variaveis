b2711e0935ba0ecd749715ab78061f9ec6e066b0
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-01-15 17:03:22 -0300

    Add files via upload
    novo desafio

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-//declaração de váriaveis
-let nome = ''
-let idade = 0
-let altura = 0
-let peso = 0
+const temFome = prompt('você tem fome? sim/não')
+const temDinheiro = prompt('você tem dinheiro? sim/não')
+const temReastauranteAberto = prompt('tem algum restaurante berto? sim/não')
 
-//solicitando informacao ao usuario e atribuindo os valores as variaveis
-nome = String(prompt("Digite seu nome: "))
-idade = parseInt(prompt("Digite sua idade: "))
-altura = parseFloat(prompt("Digite sua altura: "))
-peso = parseFloat(prompt("Digite seu peso: "))
+//
 
-//calculando o ano que a pessoa nasceu e o IMC
-let anoNasc = 0
-anoNasc = 2023 - idade
-
-let imc = 0
-imc = peso/(altura*altura)
-imcFix = imc.toFixed(1)
-
-//exibindo as informações no console
-console.log("Olá " + nome + ", você tem " + idade + " anos, nasceu em " + anoNasc + " tem " + altura + ", e seu peso é " + peso + " e o imc é " + imcFix + " kg/m ")
+if ( temFome === 'não' || temDinheiro === 'não'){
+    console.log('Hoje a janta será em casa!');
+}else if ( temFome === 'sim' && temReastauranteAberto === 'sim'){
+    console.log('A janta será no seu restaurante preferido!');
+}else {
+    console.log('Peça um delivery!')
+}
