1879bef65f6c7f4512a79398cb518ef233c6b806
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-01-19 01:11:26 -0300

    desafio tabuada

---
+++ 004.3/index.js
@@ -0,0 +1,16 @@
+let numero = Number(prompt("Insira um numero:"))
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${numero} * ${i} = ${numero * i}`)
+
+}
+numero++
+for (let i = 0; i <= 10; i++) {
+    console.log(`${numero} * ${i} = ${numero * i}`)
+
+}
+numero++
+for (let i = 0; i <= 10; i++) {
+    console.log(`${numero} * ${i} = ${numero * i}`)
+
+}
