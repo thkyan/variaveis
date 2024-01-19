7ca756601953c92f43cd21b1c381be61dfde6591
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-01-18 23:36:35 -0300

    desafio 4

---
+++ 004.1/index.js
@@ -0,0 +1,5 @@
+for (let i = 0; i <= 50; i+=5) {
+    console.log(i)
+    
+    
+}
---
+++ 004/index.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("Insira um numero que seja inteiro e positivo:"))
+for (let i = 0; i <= numero;i++) {
+    console.log(i)
+    
+    
+}
